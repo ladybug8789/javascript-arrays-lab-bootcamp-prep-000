72a44d112d4039b6756e4cffd028ba0857c09219
Sara Ledger <sara.ledger@live.com>
2018-05-17 00:52:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 const app = "I don't do much."
+
+function Arrays() {
+  window.kittens = ['Milo', 'Otis', 'Garfield']
+}
+
+function kittens() {
+  var kittens = ['Milo', 'Otis', 'Garfield']
+}
+
+function destructivelyAppendKitten('Ralph') {
+  return kittens.push('Ralph')
+}
